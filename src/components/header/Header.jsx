85e9a1b0d0aeb76790d16c2cbd47d5563cad3343
Miguel Carrillo <carrilobravom@gmail.com>
2023-06-07 16:26:55 -0500

    Se agrego pasarela y fotos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,10 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./header.css"
 import { DateRange } from 'react-date-range';
 import { useState } from "react";
-import {useNavigate, Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format}from"date-fns";
+import Modal from "../Modal/Modal";
+import { MainLogin } from "../../pages/LogIn/LogIn";
 
 const Header = ({type}) => {
     const [ciudad, setCiudad] = useState ("");
@@ -43,6 +45,15 @@ const handleSearch = ()=>{
   navigate ("/canchas", { state: {ciudad,date,options}});
 };
 
+const [modalOpen, setModalOpen] = useState(false);
+    
+const openModal = () =>{
+    setModalOpen(true)
+}
+const closeModal = () =>{
+    setModalOpen(false)
+}
+
     return(
         <div className="header">
             <div className={type ==="list" ? "headerContainer listMode": "headerContainer"}>
@@ -60,9 +71,11 @@ const handleSearch = ()=>{
             <>
             <h1 className="headerTitle">El amor esta en la cancha.</h1>
             <p className="headerDesc">Reserva tú cancha - 10% de descuento si lo haces desde tú cuenta cancheros </p>
-            <button className="headerBtn">
-            <Link className='LinkLog1' to='/LogIn'>Iniciar Sesión / Registrarse</Link>
-            </button>
+            <button className="headerBtn" onClick={openModal} to='/LogIn'>Iniciar Sesión / Registrarse</button>
+               <Modal isOpen={modalOpen} onClose={closeModal}>
+                    <MainLogin/>
+                    
+                </Modal> 
             
             <div className="headerSearch">
                 <div className="headerSearchItem">
@@ -71,21 +84,21 @@ const handleSearch = ()=>{
                     placeholder="¿De que ciudad buscas?" 
                     className="headerSearchInput"
                     onChange={e=>setCiudad(e.target.value)}/>
-                 </div>
+                </div>
                 <div className="headerSearchItem">
                     <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
                     <span onClick={() => setOpenDate(!openDate)}className="headerSearchText">{format(date[0].startDate,"MM/dd/yyyy"
                     )} to {format(date[0].endDate,"MM/dd/yyyy")}</span>
                     {openDate && <DateRange
-                     editableDateInputs={true}
-                     onChange={item => setDate([item.selection])}
-                     moveRangeOnFirstSelection={false}
-                     ranges={date}  
-                     className="date"
-                     minDate={new Date()}
-                     />}
-                 </div>
-                 <div className="headerSearchItem">
+                    editableDateInputs={true}
+                    onChange={item => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}  
+                    className="date"
+                    minDate={new Date()}
+                    />}
+                </div>
+                <div className="headerSearchItem">
                     <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
                     <span onClick={()=>setOpenOptions(!openOptions)}className="headerSearchText"> {options.persona} Personas - C futbol {options.cancha} </span>
                     {openOptions && <div className="options">
